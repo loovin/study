286c133d62a879c6e665df788bd4eabe51b65244
loovin <samboy@aliyun.com>
2018-07-03 17:22:47 +0800

    c

---
+++ lambda/src/main/java/com/tsb/study/lambda/stream/ConcurStream.java
@@ -0,0 +1,17 @@
+package com.tsb.study.lambda.stream;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.Stream;
+
+/**
+ * 并行流pa
+ */
+public class ConcurStream {
+    public static void main(String str[]){
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism","10");
+        int max = Stream.iterate(1,x->x+1).parallel().limit(200).peek(x->{
+            System.out.println(Thread.currentThread().getName());
+        }).max(Integer::compare).get();
+        System.out.println(max);
+    }
+}
